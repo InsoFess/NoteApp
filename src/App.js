4196c71dd10b0d42b7199f39d664f16c04903efb
insofess <insofess@gmail.com>
2022-08-01 07:18:55 +0300

    Create Note App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./Components/css/App.css";
+import Notes from "./Components/NoteComponents/Notes";
+import Header from "./Components/NoteComponents/Header";
+function App() {
+  return (
+    <div className="main">
+      <Header />
+      <Notes />
+    </div>
+  );
+}
+export default App;
